98899e8ad243df64d5aa11d1bd433156d07a3e46
akiram <akiramazvn@gmail.com>
2021-09-18 13:12:19 +0700

    integrate db

--- database/migrations/20210918035043_initial_migration.js
+++ database/migrations/20210918035043_initial_migration.js
@@ -1,19 +1,22 @@
 exports.up = function(knex) {
   return knex.schema
     .createTable('users', function (table) {
-      table.increments('id')
-      table.string('first_name', 255).notNullable()
-      table.string('last_name', 255).notNullable()
+      // table.increments('id')
+      table.string('user_id', 255).notNullable()
+      table.string('name', 255)
+      table.string('status_message', 512)
+      table.string('picture', 255)
+      table.string('access_token', 1024)
+      table.string('refresh_token', 1024)
+      table.string('id_token', 1024)
+      table.timestamp('created_at')
+      table.timestamp('updated_at')
+
+      table.primary(['user_id'])
     })
-    .createTable('products', function (table) {
-      table.increments('id')
-      table.decimal('price').notNullable()
-      table.string('name', 1000).notNullable()
-    }) 
 }
 
 exports.down = function(knex) {
   return knex.schema
-    .dropTable("products")
     .dropTable("users")
 }
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const axios = require('axios')
 const querystring = require('querystring')
 const dotenv = require('dotenv')
+const dateformat = require('dateformat')
 const db = require('./db-config');
 const app = express()
 const port = 5000
@@ -16,7 +17,6 @@ const url = `https://access.line.me/oauth2/v2.1/authorize?response_type=code&cli
 
 app.get('/login', (req, res) => {
   // some logic before login here
-
   
   // redirect to LINE login
   res.redirect(url)
@@ -37,16 +37,48 @@ app.get('/callback', async (req, res) => {
       })
 
     )
-    const {id_token} = tokenResponse.data
+    const {access_token, id_token} = tokenResponse.data
 
     const verifyResponse = await axios.post('https://api.line.me/oauth2/v2.1/verify', querystring.stringify({
       'id_token': id_token,
       'client_id': channel_id
     }))
 
+    const profileResponse = await axios.get('https://api.line.me/v2/profile', {
+      headers: {
+        'Authorization': `Bearer ${access_token}`
+      }
+    })
+
+    const html = `
+<pre>
+${JSON.stringify({code,state,...tokenResponse.data,...verifyResponse.data,...profileResponse.data},null,4)}
+</pre>
+<img src=${verifyResponse.data.picture} width="200">
+`.trim()
+
+    const payload = {
+      user_id: profileResponse.data.userId, 
+      name: profileResponse.data.displayName,
+      status_message: profileResponse.data.statusMessage,
+      picture: profileResponse.data.pictureUrl,
+      access_token: tokenResponse.data.access_token,
+      refresh_token: tokenResponse.data.refresh_token,
+      id_token: tokenResponse.data.id_token,
+    }
+
+    // upsert user information to DB
+    await db('users').insert({
+      ...payload,
+      created_at: dateformat(new Date(), 'isoUtcDateTime')
+    }).onConflict('user_id').merge({
+      ...payload,
+      updated_at: dateformat(new Date(), 'isoUtcDateTime')
+    })
+
     res.format({
       html: () => {
-        res.send(`<pre>${JSON.stringify({code,state,...tokenResponse.data,...verifyResponse.data},null,4)}</pre><img src=${verifyResponse.data.picture} width="200">`)
+        res.send(html)
       }
     })
   } catch (e) {
